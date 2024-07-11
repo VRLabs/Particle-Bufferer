cf6315d136d1f7e3e63f8eeff39381e66cfd635c
jellejurre <jelle@jilles.com>
2024-07-11 19:56:41 +0200

    Initial commit

---
+++ Editor/BufferParticleCreator.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRLabs.ParticleBufferer
+{
+	public class BufferParticleCreator
+	{
+		public static ParticleSystem CreateBufferParticleFromTarget(GameObject go)
+		{
+			var particle = go.GetComponent<ParticleSystem>();
+			if (particle == null) return null;
+			
+			Undo.RecordObject(go, "Create Buffer Particle");
+			go.SetActive(true);
+			var bps = CreateBufferParticle(particle);
+			bps.gameObject.name = go.name;
+			go.name += " (Sub)";
+			GameObjectUtility.EnsureUniqueNameForSibling(go);
+			EditorUtility.SetDirty(go);
+			return bps;
+		}
+		
+		public static void CreateBufferParticle(out ParticleSystem bufferParticle, out ParticleSystem subParticle)
+		{
+			var subGameObject = new GameObject("Sub Particle");
+			subParticle = subGameObject.AddComponent<ParticleSystem>();
+			
+			#region Default Sub Particle Settings
+			var main = subParticle.main;
+			main.loop = false;
+			main.playOnAwake = false;
+			
+			var shape = subParticle.shape;
+			shape.enabled = false;
+			
+			var emission = subParticle.emission;
+			emission.rateOverTime = 0;
+			emission.SetBursts(new []{new ParticleSystem.Burst(0,1)});
+			#endregion
+			
+			Undo.RegisterCreatedObjectUndo(subGameObject, "Create Buffer Particle");
+			bufferParticle = CreateBufferParticle(subParticle);
+		}
+		
+		public static ParticleSystem CreateBufferParticle(ParticleSystem subParticle)
+		{
+			ApplySubParticleSettings(subParticle);
+			var bps = CreateBufferParticleBase();
+			CopyTransformSettings(subParticle.transform, bps.transform);
+			bps.transform.rotation = Quaternion.identity;
+			GameObjectUtility.EnsureUniqueNameForSibling(bps.gameObject);
+			
+			bps.transform.SetSiblingIndex(subParticle.transform.GetSiblingIndex());
+			
+			var sem = bps.subEmitters;
+			sem.enabled = true;
+			sem.AddSubEmitter(subParticle, ParticleSystemSubEmitterType.Birth, ParticleSystemSubEmitterProperties.InheritNothing);
+			return bps;
+		}
+
+		public static ParticleSystem CreateBufferParticleBase()
+		{
+			GameObject bufferParticleObject = new GameObject("Buffer Particle");
+			bufferParticleObject.SetActive(false);
+			ParticleSystem bufferParticle = bufferParticleObject.AddComponent<ParticleSystem>();
+			Object.DestroyImmediate(bufferParticleObject.GetComponent<ParticleSystemRenderer>());
+			Undo.RegisterCreatedObjectUndo(bufferParticleObject, "Create Buffer Particle");
+			
+			#region Buffer Particle Settings
+			var main = bufferParticle.main;
+			main.duration = 1;
+			main.playOnAwake = true;
+			main.loop = false;
+			main.startLifetime = 1000;
+			main.startSpeed = 0.0001f;
+			main.maxParticles = 1;
+
+			var em = bufferParticle.emission;
+			em.rateOverTime = 0;
+			em.SetBursts(new []{new ParticleSystem.Burst(0,1)});
+			
+			var shape = bufferParticle.shape;
+			shape.enabled = false;
+			#endregion
+
+			return bufferParticle;
+		}
+
+		public static void ApplySubParticleSettings(ParticleSystem subParticle)
+		{
+			Undo.RecordObject(subParticle, "Apply Sub Particle Settings");
+			var main = subParticle.main;
+			main.loop = false;
+			main.playOnAwake = false;
+
+			var em = subParticle.emission;
+			var rot = em.rateOverTime;
+			var rod = em.rateOverDistance;
+			if (rot.mode != ParticleSystemCurveMode.Constant || rot.constantMax > 100 || rod.Evaluate(0) > 0)
+				Debug.LogWarning("Target particle system has emission values that can't be converted cleanly to burst. Please adjust manually.");
+			else if (rot.constantMax > 0)
+			{
+				var burstInterval = 1 / rot.constantMax;
+				em.rateOverTime = 0;
+
+				var bursts = new ParticleSystem.Burst[em.burstCount];
+				em.GetBursts(bursts);
+				ArrayUtility.Add(ref bursts, new ParticleSystem.Burst(0, 1, 1, 0, burstInterval));
+				em.SetBursts(bursts);
+			}
+			EditorUtility.SetDirty(subParticle);
+		}
+
+		public static void CopyTransformSettings(Transform source, Transform destination)
+		{
+			destination.parent = source.parent;
+			destination.localPosition = source.localPosition;
+			destination.localRotation = source.localRotation;
+			destination.localScale = source.localScale;
+		}
+	}
+}
---
+++ Editor/ParticleBufferer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRLabs.ParticleBufferer
+{
+	public static class ParticleBufferer
+	{
+		#region HashSet
+		// Ok so here's why this int exists. Prepare yourself.
+		// Typically with MenuItem methods, you can pass 'MenuCommand' argument to get the Object being processed currently.
+		// That's because Unity calls this method for each target of the context menu, and is SUPPOSED to pass the GameObject as the context object.
+		// BUT THAT DOESN'T WORK. IT'S ALWAYS NULL.
+		// So the only way to know what we right clicked on is to get the Selection.
+		// But because it calls it for each selected item, IT WOULD LOOP THROUGH THE SELECTION FOR THE SAME NUMBER OF SELECTED ITEMS.
+		// So we need to keep track of what we've already processed so we don't do it again.
+		// And one more thing! It calls it only once if you use the GameObject toolbar menu :) I'm not accounting for that bs.
+		// We love Unity!
+		private static int processedCount = -1;
+		#endregion
+		
+		public static bool SelectionHasParticleSystem() => Selection.gameObjects.Any(go => go != null && go.GetComponent<ParticleSystem>() != null);
+		
+		[MenuItem("GameObject/Effects/Buffer Particle/From Selected", true, 1000)]
+		public static bool CreateBufferParticleFromSelectedValidate() => SelectionHasParticleSystem();
+
+		[MenuItem("GameObject/Effects/Buffer Particle/For Each Selected", true, 1001)]
+		public static bool CreateBufferParticleForEachSelectedValidate() => SelectionHasParticleSystem();
+
+		[MenuItem("GameObject/Effects/Buffer Particle/From Selected", false, 1000)]
+		public static void CreateBufferParticleFromSelected()
+		{
+			if (processedCount < 0) processedCount = Selection.gameObjects.Length - 1;
+			if (processedCount-- != 0) return;
+			var particles = Selection.gameObjects.Select(go => go == null ? null : go.GetComponent<ParticleSystem>()).Where(ps => ps != null).ToList();
+			if (!particles.Any()) return;
+			
+			
+			ParticleSystem firstParticle = particles.Aggregate((c, d) => c.transform.GetSiblingIndex() < d.transform.GetSiblingIndex() ? c : d); // Get the lowest sibling index particle
+			
+			var bps = BufferParticleCreator.CreateBufferParticleFromTarget(firstParticle.gameObject);
+			var sem = bps.subEmitters;
+			particles.Remove(firstParticle);
+			for (int i = 0; i < particles.Count; i++)
+			{
+				BufferParticleCreator.ApplySubParticleSettings(particles[i]);
+				sem.AddSubEmitter(particles[i], ParticleSystemSubEmitterType.Birth, ParticleSystemSubEmitterProperties.InheritNothing);
+				Undo.RecordObject(particles[i].gameObject, "Create Buffer Particle");
+				particles[i].gameObject.name += $" (Sub {i + 2})";
+			}
+		}
+		
+		// Creates a buffer particle per GO with a ParticleSystem component
+		[MenuItem("GameObject/Effects/Buffer Particle/For Each Selected", false, 1001)]
+		public static void CreateBufferParticleForEachSelected()
+		{
+			if (processedCount < 0) processedCount = Selection.gameObjects.Length - 1;
+			if (processedCount-- != 0) return;
+			foreach (var go in Selection.gameObjects)
+				BufferParticleCreator.CreateBufferParticleFromTarget(go);
+		}
+
+		[MenuItem("GameObject/Effects/Buffer Particle/Empty", false, 1002)]
+		public static void CreateBufferParticleFromEmpty()
+		{
+			var targets = Selection.gameObjects.Any() ? Selection.gameObjects : new GameObject[] {null};
+			foreach (var go in targets)
+			{
+				Transform parent = go == null ? null : go.transform;
+				BufferParticleCreator.CreateBufferParticle(out var bp, out var sp);
+
+				if (parent == null) continue;
+				var t = bp.transform;
+				BufferParticleCreator.CopyTransformSettings(parent, t);
+				
+				t.parent = parent;
+
+				BufferParticleCreator.CopyTransformSettings(t, sp.transform);
+				t.transform.rotation = Quaternion.identity;
+
+				GameObjectUtility.EnsureUniqueNameForSibling(t.gameObject);
+				GameObjectUtility.EnsureUniqueNameForSibling(sp.gameObject);
+			}
+		}
+	}
+}
